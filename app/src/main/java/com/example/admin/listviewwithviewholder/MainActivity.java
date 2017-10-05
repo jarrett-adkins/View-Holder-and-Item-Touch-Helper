7b7274ea73828563a70ee11520d895e8c22abda0
jarrett-adkins <jarrett7777@gmail.com>
2017-10-04 22:54:26 -0400

    first commit

---
+++ app/src/main/java/com/example/admin/listviewwithviewholder/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.admin.listviewwithviewholder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private List<Person> personList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById( R.id.listView );
+
+        /*
+        String[] arr = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_view_item, arr));
+        */
+
+        personList = new ArrayList<>();
+
+        personList.add( new Person( "Jarrett", "Adkins" ));
+        personList.add( new Person( "Mac", "Myers" ));
+        personList.add( new Person( "Jason", "Gomez" ));
+        personList.add( new Person( "Joseph", "Casal" ));
+
+        PersonListAdapter psa = new PersonListAdapter( this, R.layout.list_view_item, personList);
+
+        listView.setAdapter( psa );
+
+    }
+}
+
+/*
+X 1. Create a list view that uses a view holder pattern.
+2. Create a recycler view that use the item touch helper class to drag and swipe items
+
+-Use different view types for items in the listView and the RecyclerView
+
+we used list view in:
+Navigation Drawer
+Saving Data
+ */
---
+++ app/src/main/java/com/example/admin/listviewwithviewholder/PersonListAdapter.java
@@ -0,0 +1,58 @@
+package com.example.admin.listviewwithviewholder;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Admin on 10/2/2017.
+ */
+
+public class PersonListAdapter extends ArrayAdapter<Person> {
+
+    public PersonListAdapter(@NonNull Context context, @LayoutRes int resource, List<Person> personList) {
+        super(context, resource, personList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        ViewHolder holder;
+
+        if( convertView == null ) {
+            //inflate the view with custom layout xml
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view_item, null);
+
+            //bind the views
+            holder = new ViewHolder();
+            holder.nameTextView = convertView.findViewById( R.id.tvPersonName );
+            holder.surnameTextView = convertView.findViewById( R.id.tvPersonSurName );
+
+            convertView.setTag(holder);
+        } else
+            holder = (ViewHolder) convertView.getTag();
+
+        //set the values
+        Person person = getItem( position );
+
+        holder.nameTextView.setText( person.getName() );
+        holder.surnameTextView.setText( person.getSurname() );
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        private TextView nameTextView;
+        private TextView surnameTextView;
+    }
+}
